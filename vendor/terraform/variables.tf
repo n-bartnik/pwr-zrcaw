805b8c5bdace605776b5da2714425e977642c6d7
Nina <73695482+n-bartnik@users.noreply.github.com>
2026-07-23 00:52:19 +0200

    Update variables.tf

--- vendor/terraform/variables.tf
+++ vendor/terraform/variables.tf
@@ -3,3 +3,9 @@ variable "lab_role" {
     description = "The role of the lab"
     type        = string
 }
+
+variable "db_password" {
+  description = "Master password for RDS"
+  type        = string
+  sensitive   = true
+}
